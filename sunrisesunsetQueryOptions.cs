245dc065009c27aa965f19e670a99ebf2c7cc4c7
APIVerve Team <hello@apiverve.com>
2024-05-14 15:42:46 -0500

    Bug fixes and Improvements

---
+++ sunrisesunsetQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class sunrisesunsetQueryOptions {
+public double lat { get; set; }
+public double lon { get; set; }
+public string date { get; set; }
+}
+
+}
