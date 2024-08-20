f811426812cdb102a7491b38ad09d83db9db6c5f
APIVerve Team <hello@apiverve.com>
2024-08-20 11:09:43 -0500

    Bug Fixes

--- SunriseSunsetAPIClient.cs
+++ SunriseSunsetAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, sunrisesunsetQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, SunriseSunsetQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(sunrisesunsetQueryOptions options = null)
+        public ResponseObj Execute(SunriseSunsetQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(sunrisesunsetQueryOptions options)
+        private string constructURL(SunriseSunsetQueryOptions options)
         {
             string url = _apiEndpoint;
 
